f31126f94fef884171b51e076c20b4c55faab4fd
Yves W. <156063351+ywerling@users.noreply.github.com>
2025-02-03 13:45:39 +0000

    Basic mathematic operators

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+	i1, i2, i3 := 13, 35, 87
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer Sum:", intSum)
+
+	f1, f2, f3 := 2.3, 5.15, 10.12
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float Sum:", floatSum)	
+
+	total := float64(i1) + f2 
+	fmt.Println("Total:", total)	
+
+	product := float64(i2) * f3
+	fmt.Println("Product:", product)	
+
+	diff := i3 - i2
+	fmt.Println("Difference:", diff)	
+
+	modulo := i3 % i1
+	fmt.Println("Modulo:", modulo)	
 }
